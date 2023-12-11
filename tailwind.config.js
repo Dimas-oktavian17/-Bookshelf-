1c6359f4b98ed443aac2c9904596db8419f92811
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-12-11 14:19:11 +0700

    init and started project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+    ],
+    theme: {
+        fontFamily: {
+            display: ['Nunito', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                primary: '#312EB5',
+                'main-bg': '#181818',
+                'secondary-bg': '#1C1C1C',
+                'secondary-child': 'rgba(255, 255, 255, 0.03)',
+                'secondary-active': 'rgba(255, 255, 255, 0.10)',
+                'main-text': '#FFF'  // if secondary add /60
+            },
+        },
+    },
+    plugins: [],
+}

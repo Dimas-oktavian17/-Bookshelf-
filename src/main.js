bd9d295bdcbc4371de6d05cd81faf3226d8d393f
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-12-15 12:40:25 +0700

    resolve production error from 'main.js'

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import { Icon } from '@iconify/vue'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import mainComponent from './components/mainComponent.vue'
-import footerComponent from './components/footerComponent.vue'
 import buttonComponent from './components/button.vue'
 import labelComponent from './components/label.vue'
 import inputComponent from './components/input.vue'
@@ -12,7 +11,6 @@ import filterRadios from './components/filterRadios.vue'
 import darkMode from './components/darkmode.vue'
 const app = createApp(App)
 app.component('IconVue', Icon)
-    .component('footerComponent', footerComponent)
     .component('mainComponent', mainComponent)
     .component('buttonComponent', buttonComponent)
     .component('labelComponent', labelComponent)
